553a67ad0258d4a4f84ff6f80683c6a2dd3cb428
xyzyuji <xyzyuji_ipod@yahoo.co.jp>
2025-06-04 12:06:50 +0900

    log update

--- logger.py
+++ logger.py
@@ -1,5 +1,7 @@
 import logging
 
 def get_logger():
+    print("===================================================")
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    print("===================================================")
     return logging.getLogger("cats_or_dogs_logger")
